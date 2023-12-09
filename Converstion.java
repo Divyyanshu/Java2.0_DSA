59ecc8aa1613eb5576b2c47d83931efdfdecc1ec
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-12-10 01:01:16 +0530

    add two files or

---
+++ Converstion.java
@@ -0,0 +1,13 @@
+// type convesrion
+
+// byte > short > int > float > long > double
+// revesre is thhis is
+// lossy convesrion
+
+public class Converstion{
+    public static void main (String args[]){
+ int a = 14;
+ float b = a;
+ System.out.println(b);
+    }
+}
