661af6513e492fe31d151c021d74ec6bbf4ce5ad
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-12-14 18:15:08 +0530

    pattern questions

--- InversePattern.java
+++ InversePattern.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class InversePattern {
     public static void main(String args[]){
-        int n = 9;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the line of pattern what you want :");
+        int n = s.nextInt();
         for (int line = 1; line<=n ;line++){
             for(int star = 1; star<= n-line+1; star++){
                 System.out.print("*");
